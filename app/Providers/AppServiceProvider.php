f30782890da0ab4d23ef2339cf3d0f86394f19ae
Rakibul Hasan <six.rakib@gmail.com>
2021-01-03 01:14:07 +0600

    Solution: Can handle multiple payment gateway request!

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 
 use App\Billing\PaymentGatewayContract;
-# use App\Billing\BankPaymentGateway;
+use App\Billing\BankPaymentGateway;
 use App\Billing\CreditPaymentGateway;
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,7 +19,16 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton( PaymentGatewayContract::class, function($app){
             # For Payment medium we just need to tell it here
             # return new BankPaymentGateway($currentcy = 'usd');
-            return new CreditPaymentGateway($currentcy = 'usd');
+            if (request()->has('credit'))
+            {   
+                # to test this use this link
+                # http://127.0.0.1:8000/pay?credit=true
+                return new CreditPaymentGateway($currentcy = 'usd');
+
+            }
+            return new BankPaymentGateway($currentcy = 'usd');
+
+            
         });
     }
 
