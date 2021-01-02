32e73bd0fdc801b1e16897565a1d744ae40231a0
Rakibul Hasan <six.rakib@gmail.com>
2021-01-02 23:06:10 +0600

    Problem: we need service provider here

--- app/Http/Controllers/PayOrderController.php
+++ app/Http/Controllers/PayOrderController.php
@@ -4,7 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Billing\PaymentGateway;
+
 class PayOrderController extends Controller
-{
-    //
+{   // Reflection Class 
+    public function store(PaymentGateway $paymentGateway )
+    {   
+        # $paymentGateway = new PaymentGateway( $currency = 'usd');
+        dd($paymentGateway->charge(2500));
+    }
 }
