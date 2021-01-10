19426bd384599aec2e6885b81a24510d481991b6
Rakibul Hasan <six.rakib@gmail.com>
2021-01-10 21:53:01 +0600

    problem-2: Composer view

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {   // view()->share('key', $value);
+        // share with every single view
         // for only when you need to do that many places asc /desc
         view()->share('channels', Channel::orderBy('name', 'asc')->get());
     }
