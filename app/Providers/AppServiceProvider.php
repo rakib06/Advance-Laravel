04d3f463a401fc2bb94dafd99a77a361c19e018e
Rakibul Hasan <six.rakib@gmail.com>
2021-01-10 21:59:46 +0600

    problem-2: Granular views with wildcards

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,9 +25,21 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {   // view()->share('key', $value);
+    {   // Option 1: every single view
+        
+        // view()->share('key', $value);
         // share with every single view
         // for only when you need to do that many places asc /desc
-        view()->share('channels', Channel::orderBy('name', 'asc')->get());
+        // view()->share('channels', Channel::orderBy('name', 'asc')->get());
+        
+
+        # Option 2: Granular views with wildcards  
+        /* view()->composer('name', function ($view) {
+            
+        }); */
+        // post.* 
+        view()->composer(['post.create'], function ($view) {
+            $view->with('channels', Channel::orderBy('name', 'desc')->get());
+        });
     }
 }
